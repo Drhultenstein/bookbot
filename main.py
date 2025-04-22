05d97c1b215daa852b62340448826c22e99305a0
Drhultenstein <ivar.hult111@gmail.com>
2025-04-22 12:23:19 +0200

    changed so it counts the words of the document

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ def get_book_text(book_path: str) -> str:
     with open(book_path) as f:
         return f.read()
     
+def count_words(text: str) -> int:
+    words = text.split()
+    return len(words)
+    
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    print(book_text)
+    print(f"{count_words(book_text)} words found in the document")
 
 
 if __name__ == "__main__":
