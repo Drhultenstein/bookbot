40ba4da73caa6a2432dc3f6d4c068036059af1e0
Drhultenstein <ivar.hult111@gmail.com>
2025-04-22 13:06:13 +0200

    added so it now counts each character

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ def get_book_text(book_path: str) -> str:
         return f.read()
     
 from stats import count_words
+from stats import count_characters
     
 def main():
     script_dir = os.path.dirname(__file__)  # folder where main.py is
     book_path = os.path.join(script_dir, "books", "frankenstein.txt")
     book_text = get_book_text(book_path)
     print(f"{count_words(book_text)} words found in the document")
+
+    char_counts = count_characters(book_text)
+    print(char_counts)
 
 
 if __name__ == "__main__":
