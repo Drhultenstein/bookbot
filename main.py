a739613b8ded90e4fc9067e5223eaad4111f3144
Drhultenstein <ivar.hult111@gmail.com>
2025-04-22 12:12:48 +0200

    added so main-py can print books

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(book_path: str) -> str:
+    with open(book_path) as f:
+        return f.read()
+    
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+
+if __name__ == "__main__":
+    main()
