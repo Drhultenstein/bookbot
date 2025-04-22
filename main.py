5c465a057227d5cc3ae179433f1c6c5dcacab781
Drhultenstein <ivar.hult111@gmail.com>
2025-04-22 13:25:09 +0200

    refactor main.py to improve output formatting and structure; add sorting functionality for character counts in stats.py

--- main.py
+++ main.py
@@ -6,16 +6,27 @@ def get_book_text(book_path: str) -> str:
     
 from stats import count_words
 from stats import count_characters
+from stats import sort_characters
     
 def main():
     script_dir = os.path.dirname(__file__)  # folder where main.py is
     book_path = os.path.join(script_dir, "books", "frankenstein.txt")
     book_text = get_book_text(book_path)
-    print(f"{count_words(book_text)} words found in the document")
 
+    word_count = count_words(book_text)
     char_counts = count_characters(book_text)
-    print(char_counts)
+    sorted_chars = sort_characters(char_counts)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_chars:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict["char"]}: {char_dict["count"]}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -11,3 +11,15 @@ def count_characters(text: str) -> dict:
         else:
             char_count[char] = 1
     return char_count
+
+def sort_characters(char_count: dict):
+    char_list = []
+    for char, count in char_count.items():
+        char_dict = {"char": char, "count": count}
+        char_list.append(char_dict)
+
+    def sort_on(dict):
+        return dict["count"]
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
